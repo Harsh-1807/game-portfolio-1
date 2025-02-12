1fbee0798f611c669d8f24a6ac3cdf6ad3781adc
Harsh-1807 <nkharshbachhav@gmail.com>
2025-02-13 01:12:20 +0530

    d

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('enter-btn').addEventListener('click', function() {
+    document.getElementById('landing').classList.add('hidden');
+    document.getElementById('content').classList.remove('hidden');
+});
